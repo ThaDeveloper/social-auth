46524f84d358610cd37a7c0105384899416ec894
Justin Ndwiga <ndwigajustin@gmail.com>
2019-10-28 13:26:19 +0300

    simple sign/signout with google

--- src/App.js
+++ src/App.js
@@ -1,26 +1,87 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+/* global gapi */
+import { gapi } from 'gapi-script';
+import React, { Component } from 'react'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  state = {
+    isSignedIn: false,
+    error: null,
+  }
+ 
+  componentDidMount() {
+
+    const successCallback = this.onSuccess.bind(this);
+    
+    window.gapi.load('auth2', () => {
+      this.auth2 = gapi.auth2.init({
+        client_id: process.env.REACT_APP_CLIENT_ID,
+      })
+
+      this.auth2.then(() => {
+        this.setState({
+          isSignedIn: this.auth2.isSignedIn.get(),
+        });
+      });
+    });    
+
+    window.gapi.load('signin2', () => {
+      // render signin button
+      var opts = {
+        width: 200,
+        height: 50,
+        client_id: process.env.REACT_APP_CLIENT_ID,
+        onSuccess: successCallback
+      }
+      gapi.signin2.render('my-signin2', opts)
+    })
+  }
+
+  onSuccess() {
+    this.setState({
+      isSignedIn: true,
+      error: null,
+    })
+  }
+
+  onLoginFailed(err) {
+    this.setState({
+      isSignedIn: false,
+      error: err,
+    })
+  }
+
+  logout = () => {
+    let auth2 = window.gapi && window.gapi.auth2.getAuthInstance();
+    if (auth2) {
+      auth2
+        .signOut()
+        .then(() => {
+          this.setState({
+            isSignedIn: false,
+          })
+          console.log('Logged out successfully');
+        })
+        .catch(err => {
+          console.log('Error while logging out', err);
+        });
+    } else {
+      console.log('Error while logging out');
+    }
+  };
+  
+  render() {
+    return (
+      <div className="App">
+        <div id="my-signin2"></div>
+        {this.state.isSignedIn && (
+        <button style={{ width: 200, height: 40, textAlign: 'center' }} onClick={this.logout}>
+          Logout
+        </button>
+      )}
+      </div>
+     
+    )
+  }
 }
 
-export default App;
+export default App
