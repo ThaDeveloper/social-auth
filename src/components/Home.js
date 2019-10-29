1337629ac6a682fa333bd83100df1cfe5151f486
Justin Ndwiga <ndwigajustin@gmail.com>
2019-10-29 19:41:12 +0300

    add App bar

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,7 +1,27 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
+
+import { withStyles } from '@material-ui/styles';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+import Button from '@material-ui/core/Button';
+import IconButton from '@material-ui/core/IconButton';
+import MenuIcon from '@material-ui/icons/Menu';
+
 import { logoutUser } from "../actions";
 
+const styles = theme => ({
+    root: {
+      flexGrow: 1,
+    },
+    menuButton: {
+      marginRight: "0.5%",
+    },
+    title: {
+      flexGrow: 1,
+    },
+  });
 
 class Home extends Component {
   handleLogout = () => {
@@ -9,15 +29,26 @@ class Home extends Component {
     dispatch(logoutUser());
   };
   render() {
-    const { isLoggingOut, logoutError, user } = this.props;    
+    const { classes } = this.props;
+    const { isLoggingOut, logoutError, user } = this.props; 
     return (
-      <div>
-        <h1>Welcome {user.displayName}</h1>
-        {console.log({user})}
-        <button onClick={this.handleLogout}>Logout</button>
-        {isLoggingOut && <p>Logging Out....</p>}
-        {logoutError && <p>Error logging out</p>}
-      </div>
+        <div className={classes.root}>
+            <AppBar position="static">
+                <Toolbar>
+                    <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="menu">
+                        <MenuIcon />
+                    </IconButton>
+                    <Typography variant="h6" className={classes.title}>
+                        All Social
+                    </Typography>
+                    <Button color="inherit" onClick={this.handleLogout}>Logout</Button>
+                </Toolbar>
+            </AppBar>
+            <h1>Welcome {user.displayName}</h1>
+            {console.log({user})}
+            {isLoggingOut && <p>Logging Out....</p>}
+            {logoutError && <p>Error logging out</p>}
+        </div>
     );
   }
 }
@@ -28,4 +59,4 @@ function mapStateToProps(state) {
     user: state.auth.user
   };
 }
-export default connect(mapStateToProps)(Home);
+export default connect(mapStateToProps)((withStyles(styles)(Home)));
